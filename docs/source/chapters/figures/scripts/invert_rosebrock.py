19c311690b4397a6512a8036081f1fc2f0f2aa7a
Lucas Sawade <lsawade@princeton.edu>
2020-10-16 15:35:02 -0400

    Added invert Rosenbrock function

---
+++ docs/source/chapters/figures/scripts/invert_rosebrock.py
@@ -0,0 +1,10 @@
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Internal
+from lwsspy import DOCFIGURES  # Location to store figure
+from lwsspy import updaterc    # Makes figure pretty in my opinion
+updaterc()
+
+
