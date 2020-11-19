ab786f60b47d6c30782743cb8e3a4743cbe20c17
Lucas Sawade <lsawade@princeton.edu>
2020-11-18 20:00:27 -0500

    Added a bunch of functions

---
+++ docs/source/chapters/figures/scripts/project2D.py
@@ -0,0 +1,48 @@
+from matplotlib.patches import ConnectionPatch
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Internal
+import lwsspy as lpy
+from lwsspy import DOCFIGURES  # Location to store figure
+from lwsspy import updaterc    # Makes figure pretty in my opinion
+
+lpy.updaterc()
+
+# Create data
+x0 = np.array([0, 2, 2, 0])
+y0 = np.array([0, 0, 2, 2])
+xn = np.array([0, 4, 5, 2])
+yn = np.array([0, 1, 3, 4])
+x0q = np.array([0.5, 1.5, 1.5, 0.5])
+y0q = np.array([0.5, 0.5, 1.5, 1.5])
+
+xnq, ynq = lpy.project2D(x0, y0, xn, yn, x0q, y0q)
+
+# Plot figure
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4.5))
+plt.subplots_adjust(bottom=0.275, wspace=0.25)
+ax1.plot(np.append(x0, x0[0]), np.append(y0, y0[0]),
+         'kx-', label="0-Coordinate-System-Basis-Nodes")
+ax1.plot(np.append(x0q, x0q[0]), np.append(y0q, y0q[0]),
+         'ko-', label="Query-Points in 0-CS")
+ax1.set_xlabel('x')
+ax1.set_ylabel('y')
+ax2.plot(np.append(xn, xn[0]), np.append(yn, yn[0]),
+         'x-', label="N-Coordinate-System-Basis-Nodes")
+ax2.plot(np.append(xnq, xnq[0]), np.append(ynq, ynq[0]),
+         'o-', label="Resulting points in N-CS")
+ax2.set_xlabel('x')
+ax2.set_ylabel('y')
+
+# Show transformation
+for _x0, _y0, _xn, _yn in zip(x0q, y0q, xnq, ynq):
+    con = ConnectionPatch(xyA=(_x0, _y0), axesA=ax1, coordsA="data",
+                          xyB=(_xn, _yn), axesB=ax2, coordsB="data")
+    fig.add_artist(con)
+fig.legend(loc=8)
+
+# Save figure with rasterization with 300dpi
+plt.savefig(os.path.join(lpy.DOCFIGURES, "project2D.svg"), dpi=300)
+plt.show()
--- lwsspy/plot_util/plot_mesh.py
+++ lwsspy/plot_util/plot_mesh.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pyvista as pv
+import math
+import lwsspy as lpy
 
 
 def plot_mesh(mesh: pv,
@@ -57,7 +59,7 @@ def plot_mesh(mesh: pv,
     p.add_slider_widget(
         my_plane_funcy, value=0, title='Z', rng=[ymin, ymax],
         pointa=(.68, .1), pointb=(.93, .1))
-    p.show_bounds(bounds=mesh.bounds, location='back')
+    p.show_bounds(bounds=mesh.bounds, location='origin')
     # p.show_grid()
     # p.add_axes()
     p.show()
@@ -83,7 +85,8 @@ class MeshPlot():
 
     def __init__(self, mesh,
                  rotlat: float or None = None,
-                 rotlon: float or None = None):
+                 rotlon: float or None = None,
+                 get_mean: bool = False):
         self.mesh = mesh.copy(deep=True)  # Expected PyVista mesh type
 
         if rotlon is not None:
@@ -93,87 +96,96 @@ class MeshPlot():
             pv.core.common.axis_rotation(
                 self.mesh.points, rotlat, inplace=True, deg=True, axis='y')
 
+        # Get bounds
+        self.xmin, self.xmax, self.ymin, self.ymax, self.zmin, self.zmax = \
+            self.mesh.bounds
+
+        # Get the vector meanposition of your dataset
+        if get_mean:
+            self.initpos = np.mean(self.mesh.points, axis=0)
+        else:
+            # Set initial position and normal
+            self.initpos = np.array([1, 0, 0])
+
+        _, self.latitude, self.longitude = lpy.cart2geo(*self.initpos)
+
+        self.center = self.initpos
+        self.normaly = np.array([0, 1, 0])
+        self.normalz = np.array([0, 0, 1])
+
         # default parameters
         self.rotation_angle = 0.0
-        self.x = 0.0
-        self.y = 0.0
-        self.z = 0.0
-        self.normalx = [1, 0, 0]
-        self.normaly = [0, 1, 0]
-        self.normaly = [0, 0, 1]
+        # self.x = (self.xmax + self.xmin)/2
+        # self.y = (self.ymax + self.ymin)/2
+        # self.z = (self.zmax + self.zmin)/2
+        self.center = np.array([1, 0, 0])
+
         self.rotmat = np.eye(3)
-        self.rotmatx = np.eye(3)
-        self.rotmaty = np.eye(3)
-        self.rotmatz = np.eye(3)
+        self.rotmat_lat = np.eye(3)
+        self.rotmat_lon = np.eye(3)
+        self.rotmat_slice = np.eye(3)
 
         # Initiate Plotter
-        p = pv.Plotter()
-        xmin, xmax, ymin, ymax, zmax, zmin = mesh.bounds
+        self.p = pv.Plotter()
 
-        p.add_mesh(mesh, opacity=0.1)
+        self.p.add_mesh(self.mesh, opacity=0.1)
 
-        p.add_slider_widget(
-            self.my_plane_funcx, value=0, title='X', rng=[xmin, xmax],
-            pointa=(.025, .1), pointb=(.31, .1),
-            style='modern')
-        p.add_slider_widget(
-            self.my_plane_funcy, value=0, title='Y', rng=[ymin, ymax],
-            pointa=(.35, .1), pointb=(.64, .1))
+        # Rotation Widgets
+        self.p.add_slider_widget(
+            self.rotate_lat, value=self.latitude, title='Lat', rng=[-90, 90],
+            pointa=(.025, .35), pointb=(.175, .35))
+        self.p.add_slider_widget(
+            self.rotate_lon, value=self.longitude, title='Lon', rng=[-180, 180],
+            pointa=(.025, .2), pointb=(.175, .2))
+        self.p.add_slider_widget(
+            self.rotate_slice, value=0, title='Rot', rng=[0, 90],
+            pointa=(.025, .05), pointb=(.175, .05))
+
         # p.add_slider_widget(
         #     self.my_plane_funcy, value=0, title='Z', rng=[ymin, ymax],
         #     pointa=(.68, .1), pointb=(.93, .1))
-        p.show_bounds(bounds=mesh.bounds, location='back')
-        
+        self.p.show_bounds(bounds=mesh.bounds, location='origin')
 
-    def my_plane_funcx(self, loc):
-        # Compute normal from
-        coangle = np.pi/2 - np.arccos(loc)
-        loc1x = np.cos(coangle)
-        loc1z = np.sin(coangle)
-        normal = [loc1x, 0, loc1z]
-        normal = normal/np.linalg.norm(normal)
+        self.p.show()
+
+    def my_plane_funcy(self):
 
         # Rotation matrix of the normal
-        normal = self.rotmat @ normal
-        # Compute Slice
-        slc = mesh.slice(normal=normal, origin=[0, 0, 0])
-        p.add_mesh(slc, name='Xslice')
-
-    def my_plane_funcy(loc):
-        # Compute normal from
-        coangle = np.pi/2 - np.arccos(loc)
-        loc1x = np.cos(coangle)
-        loc1z = np.sin(coangle)
-        normal = [0, loc1x, loc1z]
-        normal = normal/np.linalg.norm(normal)
+        normal = self.rotmat @ self.normaly
 
         # Compute Slice
-        slc = mesh.slice(normal=normal, origin=[0, 0, 0])
-        p.add_mesh(slc, name='Yslice')
+        slc = self.mesh.slice(normal=normal, origin=[0, 0, 0])
+        self.p.add_mesh(slc, name='Yslice')
 
-    def rotatex(self, angle):
-        self.rotmatx = rotation_matrix([1, 0, 0], angle)
+    def my_plane_funcz(self):
+
+        # Rotation matrix of the normal
+        normal = self.rotmat @ self.normalz
+
+        # Compute Slice
+        slc = self.mesh.slice(normal=normal, origin=[0, 0, 0])
+        self.p.add_mesh(slc, name='Zslice')
+
+    def rotate_lat(self, latitude):
+        self.latitude = latitude
+        self.center = lpy.geo2cart(1.0, self.latitude, self.longitude)
+        self.rotmat_lat = rotation_matrix(
+            [0, 1, 0], -self.latitude/180.0*np.pi)
+        self.update_rotmat()
+
+    def rotate_lon(self, longitude):
+        self.longitude = longitude
+        self.center = lpy.geo2cart(1.0, self.latitude, self.longitude)
+        self.rotmat_lon = rotation_matrix(
+            [0, 0, 1], self.longitude/180.0*np.pi)
+        self.update_rotmat()
+
+    def rotate_slice(self, angle):
+        angle = angle/180.0*np.pi
+        self.rotmat_slice = rotation_matrix(list(self.center), angle)
         self.update_rotmat()
 
     def update_rotmat(self):
-        self.rotmat = self.rotmatx @ self.rotmaty @ self.rotmatz
-
-    
-
-    if rotlon is not None:
-        pv.core.common.axis_rotation(
-            mesh.points, rotlon, inplace=True, deg=True, axis='z')
-    if rotlat is not None:
-        pv.core.common.axis_rotation(
-            mesh.points, rotlat, inplace=True, deg=True, axis='y')
-
-    def my_plane_funcz(loc):
-        slc = mesh.slice(normal=[0, 1, 0], origin=[0, loc, 0])
-        p.add_mesh(slc, name='Zslice')
-
-    def rotate(angle):
-
-    
-    # p.show_grid()
-    # p.add_axes()
-    p.show()
+        self.rotmat = self.rotmat_lon @ self.rotmat_lat @ self.rotmat_slice
+        self.my_plane_funcy()
+        self.my_plane_funcz()
