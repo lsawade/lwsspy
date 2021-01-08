cb466d0280f8689185594a0f0f358f485446edad
Lucas Sawade <lsawade@princeton.edu>
2021-01-07 22:05:04 -0500

    updated the spherical nearest neighbor function to be better..

--- lwsspy/math/SphericalNN.py
+++ lwsspy/math/SphericalNN.py
@@ -167,10 +167,10 @@ class SphericalNN(object):
                     d, np.nan)
 
             # Check nan rows
-            nanrows = np.sum(np.logical_not(np.isnan(d)), axis=1)
+            nanrows = np.sum(np.logical_not(np.isnan(d)), axis=1) == 0
 
             # Get the max of each row that has a at least one non-nan
-            rowmax = np.where(nanrows == 0, np.nanmax(d, axis=1), np.nan)
+            rowmax = np.where(nanrows != 0, np.nanmax(d, axis=1), np.nan)
 
             # Compute weights
             w = np.where(nanrows[:, np.newaxis] != 0,
