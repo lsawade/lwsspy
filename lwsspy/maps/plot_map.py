8dde43d4ef9c970f1b7e376d6fa59bf7c7f254b7
Lucas Sawade <lsawade@princeton.edu>
2021-01-07 21:49:30 -0500

    updated the spherical nearest neighbor function to be better..

--- lwsspy/maps/plot_map.py
+++ lwsspy/maps/plot_map.py
@@ -4,7 +4,8 @@ from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
 from cartopy.crs import PlateCarree, Mollweide
 
 
-def plot_map(fill=True, zorder=-10, projection=PlateCarree()):
+def plot_map(fill=True, zorder=-10, projection=PlateCarree(),
+             labelstopright: bool = True, labelsbottomleft: bool = True):
     """Plots map into existing axes.
 
     Parameters
@@ -13,17 +14,26 @@ def plot_map(fill=True, zorder=-10, projection=PlateCarree()):
         fills the continents in light gray, by default True
     zorder : int, optional
         zorder of the map, by default -10
+    projection : cartopy.crs.projection, optional
+        projection to be used for the map.
+    labelstopright : bool, optional
+        flag to turn on or off the ticks
+    labelsbottomleft : bool, optional
+        flag to turn on or off the ticks
 
     Returns
     -------
     matplotlib.pyplot.Axes
         Axes in which the map was plotted
 
+    Notes
+    -----
+
     :Author:
         Lucas Sawade (lsawade@princeton.edu)
 
     :Last Modified:
-        2020.09.17 16.30
+        2020.01.07 16.30
 
 
     """
@@ -37,11 +47,14 @@ def plot_map(fill=True, zorder=-10, projection=PlateCarree()):
 
     # Put lables all around
     if projection is not Mollweide():
-        ax.tick_params(labeltop=True, labelright=True)
 
         # Set label formatter
         ax.xaxis.set_major_formatter(cartopy.mpl.ticker.LongitudeFormatter())
         ax.yaxis.set_major_formatter(cartopy.mpl.ticker.LatitudeFormatter())
+
+        ax.tick_params(
+            labeltop=labelstopright, labelright=labelstopright,
+            labelbottom=labelsbottomleft, labelleft=labelsbottomleft)
 
     # Set gridlines
     # gl = ax.gridlines(draw_labels=False, linewidth=1, color='lightgray',
