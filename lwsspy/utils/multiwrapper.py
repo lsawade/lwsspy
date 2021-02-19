2d97d1e2d5e6cca389978ff60a9d33921ea99ac4
Lucas Sawade <lsawade@princeton.edu>
2021-02-18 23:58:54 -0500

    changed Parallel

--- lwsspy/utils/multiwrapper.py
+++ lwsspy/utils/multiwrapper.py
@@ -67,4 +67,4 @@ def starmap_with_kwargs(
     args_for_starmap = zip(repeat(fn), args_iter, kwargs_iter)
     return pool.starmap(
         apply_args_and_kwargs,
-        tqdm.tqdm(args_for_starmap, len(args_for_starmap)))
+        tqdm.tqdm(args_for_starmap, len(args_iter)))
