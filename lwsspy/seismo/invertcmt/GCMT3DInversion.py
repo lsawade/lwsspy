5df247d36800dfc2ee956c1efe4892142528922c
Lucas Sawade <lsawade@princeton.edu>
2021-03-18 23:48:10 -0400

    Changed rotation function...`

--- lwsspy/seismo/invertcmt/GCMT3DInversion.py
+++ lwsspy/seismo/invertcmt/GCMT3DInversion.py
@@ -260,12 +260,13 @@ class GCMT3DInversion:
             else:
                 channel_removal_set.intersection(
                     set(zero_window_removal_dict[_wtype]))
-
+        print(channel_removal_set)
+        print(self.zero_window_removal_dict[_wtype])
         # Remove the set from the window removal dicts
         for _i, _wtype in enumerate(self.data_dict.keys()):
             self.zero_window_removal_dict[_wtype] = \
                 set(zero_window_removal_dict[_wtype]) - channel_removal_set
-            print(self.zero_window_removal_dict[_wtype])
+
             for (net, sta, loc, cha) in self.zero_window_removal_dict[_wtype]:
                 st = self.data_dict[_wtype].select(
                     network=net, station=sta, location=loc, channel=cha)[0]
