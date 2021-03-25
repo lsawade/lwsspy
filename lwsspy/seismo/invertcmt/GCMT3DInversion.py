fixed normalization for no window traces

lwsspy/seismo/invertcmt/GCMT3DInversion.py
@@ -472,9 +472,8 @@ class GCMT3DInversion:
             self.zero_trace_array = np.array([1.0 if _par in checklist else 0.0
                                               for _par in self.pardict.keys()])
             self.zero_trace_index_array = [
-                _i if _par in checklist
-                for _i, _par in enumerate(self.pardict.keys())
-            ]
+                _i for _i, _par in enumerate(self.pardict.keys())
+                if _par in checklist]
             self.zero_trace_array = np.append(self.zero_trace_array, 0.0)
 
         # Get the model vector given the parameters to invert for