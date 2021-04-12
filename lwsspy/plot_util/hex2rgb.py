807de75c580d136eeac49a8e54a9264918538539
Lucas Sawade <lsawade@princeton.edu>
2021-04-12 06:18:36 -0400

    Added some functions

---
+++ lwsspy/plot_util/hex2rgb.py
@@ -0,0 +1,10 @@
+
+
+def hex_to_rgb(value):
+    '''
+    Converts hex to rgb colours
+    value: string of 6 characters representing a hex colour.
+    Returns: list length 3 of RGB values'''
+    value = value.strip("#")  # removes hash symbol if present
+    lv = len(value)
+    return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
