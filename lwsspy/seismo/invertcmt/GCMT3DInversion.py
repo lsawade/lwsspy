a0f16f82a67914237d01d099578ce7d2067fd611
Lucas Sawade <lsawade@princeton.edu>
2021-04-22 00:00:21 -0400

    fixed processing dict issue

--- lwsspy/seismo/invertcmt/GCMT3DInversion.py
+++ lwsspy/seismo/invertcmt/GCMT3DInversion.py
@@ -469,6 +469,7 @@ class GCMT3DInversion:
             for _component, _cweight in self.weights_rtz.items():
                 RTZ_traces[_component] = []
                 for _tr in _stream:
+                    print(_component, _tr.stats.component, _tr.stats.weights)
                     if _tr.stats.component == _component \
                             and "weights" in _tr.stats:
                         RTZ_traces[_component].append(_tr)
