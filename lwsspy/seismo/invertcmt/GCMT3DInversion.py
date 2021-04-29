34fcec0680fadc6b2bfc0432399bc83678ab7e2c
Lucas Sawade <lsawade@princeton.edu>
2021-04-29 03:08:10 -0400

    Updated inversion class

--- lwsspy/seismo/invertcmt/GCMT3DInversion.py
+++ lwsspy/seismo/invertcmt/GCMT3DInversion.py
@@ -1693,16 +1693,17 @@ def plot_seismograms(obsd: Trace, synt: Union[Trace, None] = None,
 
 def bin():
 
+    import sys
+    event = sys.argv[1]
     # Inputs
-    event = "C201711191509A"
     database = "/gpfs/alpine/geo111/scratch/lsawade/testdatabase"
     specfemdir = "/gpfs/alpine/geo111/scratch/lsawade/SpecfemMagic/specfem3d_globe"
     launch_method = "jsrun -n 6 -a 4 -c 4 -g 1"
 
     gcmt3d = GCMT3DInversion(event, database, specfemdir, pardict=pardict,
-                             download_data=True,
-                             overwrite=False, launch_method=launch_method,
-                             damping=0.001)
+                             download_data=True, zero_trace=False,
+                             duration=7200, overwrite=False,
+                             launch_method=launch_method, damping=0.0001)
     gcmt3d.init()
     gcmt3d.process_data()
     gcmt3d.get_windows()
@@ -1714,7 +1715,7 @@ def bin():
     optim_list = []
 
     max_iter = 5
-    max_nls = 4
+    max_nls = 3
 
     with lpy.Timer():
 
@@ -1740,59 +1741,12 @@ def bin():
 
         optim_list.append(deepcopy(optim_out))
 
-        # # BFGS
-        # gcmt3d.__init_model_and_scale__()
-        # lpy.print_bar("BFGS")
-        # optim_bfgs = lpy.Optimization("bfgs")
-        # optim_bfgs.compute_cost_and_gradient = gcmt3d.compute_cost_gradient
-        # optim_bfgs.is_preco = False
-        # optim_bfgs.niter_max = max_iter
-        # optim_bfgs.nls_max = max_nls
-        # optim_bfgs.stopping_criterion = 9.5e-1
-        # optim_bfgs.n = len(gcmt3d.model)
-
-        # # Run optimization
-        # optim_out = gcmt3d.optimize(optim_bfgs)
-
-        # # Update model and write model
-        # gcmt3d.__update_cmt__(optim_out.model)
-        # gcmt3d.cmt_out.write_CMTSOLUTION_file(
-        #     f"{gcmt3d.cmtdir}/{gcmt3d.cmt_out.eventname}_BFGS")
-
-        # optim_list. append(deepcopy(optim_out))
-
-    #     # # Regularized Gauss Newton
-    #     gcmt3d.damping = 0.1
-    #     gcmt3d.__init_model_and_scale__()
-    #     lpy.print_bar("Gauss-Newton Regularized")
-    #     optim_gnr = lpy.Optimization("gn")
-    #     optim_gnr.compute_cost_and_grad_and_hess = gcmt3d.compute_cost_gradient_hessian
-    #     optim_gnr.is_preco = False
-    #     optim_gnr.niter_max = max_iter
-    #     optim_gnr.nls_max = max_nls
-    #     optim_gnr.alpha = 1.0
-    #     optim_gnr.stopping_criterion = 9.5e-1
-    #     optim_gnr.n = len(gcmt3d.model)
-
-    #     # Run optimization
-    #     with lpy.Timer():
-    #         optim_out = gcmt3d.optimize(optim_gnr)
-    #         lpy.print_action("DONE with Regularized Gauss-Newton")
-
-    #     # Update model and write model
-    #     gcmt3d.__update_cmt__(optim_out.model)
-    #     gcmt3d.cmt_out.write_CMTSOLUTION_file(
-    #         f"{gcmt3d.cmtdir}/{gcmt3d.cmt_out.eventname}_GNR")
-
-    #     optim_list. append(deepcopy(optim_out))
-
     # # Write PDF
     plt.switch_backend("pdf")
     lpy.plot_model_history(
         optim_list,
         list(pardict.keys()),  # "BFGS-R" "BFGS",
         outfile=f"{gcmt3d.cmtdir}/InversionHistory_2params.pdf")
-    print(gcmt3d.scale)
     lpy.plot_optimization(
         optim_list,
         outfile=f"{gcmt3d.cmtdir}/misfit_reduction_history.pdf")
