d8b60c24f4dd6ade943adbc734fa1fd431faa0dd
Lucas Sawade <lsawade@princeton.edu>
2021-06-20 02:42:24 -0400

    changed the asyncio fucntion slightly

--- lwsspy/seismo/invertcmt/sync_data.py
+++ lwsspy/seismo/invertcmt/sync_data.py
@@ -43,9 +43,10 @@ async def sync_data(
             # Create task for asyncio
             print("[INFO]     --> syncing {event} ...")
             process = await asyncio.create_subprocess_shell(command)
+            processes.append(processes)
 
         # Run two asyncio processes at the same time with asyncio
-        await asyncio.gather(*(process.communicate() for _proc in processes))
+    await asyncio.gather(*(process.communicate() for _proc in processes))
 
 
 def bin():
