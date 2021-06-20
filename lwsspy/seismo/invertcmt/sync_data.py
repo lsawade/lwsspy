1aaec85ede4951f24c36251f1a1cee2d6538f88a
Lucas Sawade <lsawade@princeton.edu>
2021-06-20 02:00:19 -0400

    Updated the asyncio sync data function to limit threads

--- lwsspy/seismo/invertcmt/sync_data.py
+++ lwsspy/seismo/invertcmt/sync_data.py
@@ -3,9 +3,19 @@ import asyncio
 from typing import List, Optional
 
 
+async def gather_with_concurrency(n, *tasks):
+    semaphore = asyncio.Semaphore(n)
+
+    async def sem_task(task):
+        async with semaphore:
+            return await task
+    return await asyncio.gather(*(sem_task(task) for task in tasks))
+
+
 async def sync_data(
         data_database: str, new_database: str,
-        eventlist: Optional[List[str]] = None):
+        eventlist: Optional[List[str]] = None,
+        n: int = 50):
 
     # If no list is provided, the entire database will be synchronized
     if eventlist is None:
@@ -30,7 +40,8 @@ async def sync_data(
         processes.append(process)
 
     # Run two asyncio processes at the same time with asyncio
-    await asyncio.gather(*(_proc.communicate() for _proc in processes))
+    await gather_with_concurrency(
+        n, *(_proc.communicate() for _proc in processes))
 
 
 def bin():
@@ -48,7 +59,11 @@ def bin():
         '-e', '--event-list', dest='event_list', nargs='+',
         help='List of events to sync', default=None,
         required=False)
+    parser.add_argument(
+        '-n', '--max-threads', dest='threads', type=int,
+        help='Maximum number of concurrent tasks', default=50,
+        required=False)
     args = parser.parse_args()
 
     asyncio.run(sync_data(args.data_database,
-                          args.new_database, args.event_list))
+                          args.new_database, args.event_list, n=args.threads))
