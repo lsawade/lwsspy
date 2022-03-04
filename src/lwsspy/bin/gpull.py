c3da03a2b69d2991f425abb6841b640bac3e14a8
Lucas Sawade <lsawade@princeton.edu>
2022-03-04 11:03:35 -0500

    made function more versatile

--- src/lwsspy/bin/gpull.py
+++ src/lwsspy/bin/gpull.py
@@ -1,13 +1,8 @@
-import os
-from subprocess import check_call
-import socket
-from sys import argv, exit
-
 """
 
 Usage: 
 
-    gpull-<remote> <local/path> <remote/path>
+    gpull <remote> <local/path> <remote/path>
 
 This file defines multiple scripts that sync directories from a specfic host 
 with a local one using rsync. Gpull works better in the sense that it is really 
@@ -16,41 +11,63 @@ circumvents this issue.
 
 Example:
 
-    gpull-tiger <local/path> <path/on/tiger/to/dir>
+    gpull tiger testdir .
 
+Uses rsync to pull changes from 
+    <puid>@tigergpu.princeton.edu":testdir 
+to 
+    ./testdir
+
+
+Note:
+
+    I have hardcoded my username. If you have multiple names across devices you
+    could create another command line argument that needs to be provided.
+
+--- 
 
 :Author:
     Lucas Sawade (lsawade-at-princeton.edu)
 
 :Last Modified:
-    2021.01.17 11.45
+    2022.03.04 11.00
 
 """
 
+from subprocess import check_call
+from sys import argv, exit
 
-def gpull(remote, remotepath, path='.'):
+
+def sub_gpull(remote, remotepath, path='.'):
 
     # User IDs
     userid = 'lsawade'
 
     # Define command
     cmd = f'rsync -a --exclude=.git {userid}@{remote}:{remotepath} {path}'
+    print(cmd)
 
     # Execute
     check_call(cmd, shell=True)
 
 
-def gpull_tiger():
+def gpull():
 
     # Get command line arguments
     # Check if argument is given
-    if len(argv) != 3:
+    if (len(argv) != 3) or (argv[1] == '-h') or (argv[1] == '--help'):
         print(__doc__)
         exit()
     else:
-        remotepath, path = argv[1:]
+        remote, remotepath, path = argv[1:]
 
     # Define fixed tiger location
-    remote = 'tigergpu.princeton.edu'
+    if remote == 'tiger':
+        remote = 'tigergpu.princeton.edu'
+    elif remote == 'traverse':
+        remote = 'traverse.princeton.edu'
+    # Assume it's a custom remote location
+    else:
+        pass
 
-    gpull(remote, remotepath, path)
+    sub_gpull(remote, remotepath, path)
