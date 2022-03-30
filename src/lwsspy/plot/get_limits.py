87fcc28f6fde7cf9ffa45085f627b8981dbadda5
Lucas Sawade <lsawade@princeton.edu>
2022-03-30 00:22:27 -0400

    fixed issue where lines were checked that didnt have dataa

--- src/lwsspy/plot/get_limits.py
+++ src/lwsspy/plot/get_limits.py
@@ -19,9 +19,17 @@ def get_limits(ax: matplotlib.axes.Axes):
     # Get the limits of each one of them
     xmins, xmaxs = [], []
     ymins, ymaxs = [], []
+
     for _line in lines:
+        # Get the data vals
         x = _line.get_xdata()
         y = _line.get_ydata()
+
+        # Skip if objection doesnt have data points
+        if len(x) == 0 or len(y) == 0:
+            continue
+
+        # Otherwise append stuff
         xmins.append(np.min(x))
         xmaxs.append(np.max(x))
         ymins.append(np.min(y))
