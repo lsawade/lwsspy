fixed optimal transport factor of 2 which was missing in the arc normalization jacobian

src/lwsspy/OT/fingerprint.py
@@ -142,13 +142,13 @@ def fingerprint_and_diff(t: np.ndarray, u: np.ndarray, nt: int, nu: int):
             dddukp1 = np.dot(dddx, dxdukp1)
 
             # C.9 Error in this equation it should be u(lambda)
-            # dij_k[i, j] = (1-lmds[kl])/d[i, j] * (uclose - pu[j])
+            # dij_k[i, j] = (1-lmds[kl])/d[i, j] * (xclose[1, kl] - pu[j])
             # dij_k[i, j] = (1-lmds[kl])/d[i, j] * (u[kl] - pu[j])
             dij_k[i, j] = ddduk
 
             # C.10 Error in this equation it should be u(lambda)
-            # dij_kp1[i, j] = lmds[kl]/d[i, j] * (uclose - pu[j])
-            # dij_kp1[i, j] = lmds[kl]/d[i, j]s * (u[kl+1] - pu[j])
+            # dij_kp1[i, j] = lmds[kl]/d[i, j] * (xclose[1, kl+1] - pu[j])
+            # dij_kp1[i, j] = lmds[kl]/d[i, j] * (u[kl+1] - pu[j])
             dij_kp1[i, j] = dddukp1
 
     return pt, pu, d, lambdas, dij_k, dij_kp1, idk
@@ -169,12 +169,12 @@ def compute_dxduk(xk: np.ndarray, xkp1: np.ndarray, p: np.ndarray, lmd: float):
         x(lambda, xk, xk+1) = (1-lambda) * xk + lamdba * xkp1
 
     Some definitions
-        lamda = g()/h(),            
+        lamda = g()/h()
     where
         g = (p - xk) dot (xk+1 - xk)
         h = || xk+1 - xk ||^2.
 
-    so dlam/duk = dgduk * h - g * dhduk
+    so dlam/duk = (dgduk * h - g * dhduk)/h^2
     """
 
     # Get difference
@@ -185,16 +185,21 @@ def compute_dxduk(xk: np.ndarray, xkp1: np.ndarray, p: np.ndarray, lmd: float):
     h = np.dot(dx, dx)
 
     # Derivatives of the numerator and denominator as a function of uk
-    dgduk = - ((xkp1[1] - xk[1]) + (p[1] - xk[1]))
-    dhduk = 2 * (xk[1] - xkp1[1])
+    dgduk = - (2*xk[1] - xkp1[1] - p[1])
+    dhduk = - 2 * (xkp1[1] - xk[1])
 
     # Derivatives of the numerator and denominator as a function of uk
     dgdukp1 = (p[1] - xk[1])
     dhdukp1 = 2 * (xkp1[1] - xk[1])
 
     # Get derivatives of lambda wrt. uk and ukp1
-    dlmdduk = (dgduk * h - g * dhduk)/h**2
-    dlmddukp1 = (dgdukp1 * h - g * dhdukp1)/h**2
+    if (lmd == 0.0) or (lmd == 1.0):
+        dlmdduk = 0
+        dlmddukp1 = 0
+
+    else:
+        dlmdduk = (dgduk * h - g * dhduk)/h**2
+        dlmddukp1 = (dgdukp1 * h - g * dhdukp1)/h**2
 
     # Get derivatives of x wrt. lamda uk and ukp1. Here it's important to note
     # x is directly dependent on uk and ukp1, but also on lambda which itself is

src/lwsspy/OT/opt.py
@@ -1,3 +1,4 @@
+from scipy.signal import hilbert as analytic_signal
 import numpy as np
 from lwsspy.wavelets import ricker
 from lwsspy.OT.ot import Waveform
@@ -10,17 +11,17 @@ def create_OT_Waveform():
 
     # General waveform parameters
     N = 500
-    L = 2
 
     # Observed/Target
     tc0 = 0
-    f00 = 1.6
+    fc0 = 1.6
+    L0 = 2.0
     A0 = 1.0
     tt = np.linspace(-2, 2, N)
 
     # Real model
-    m_real = [tc0, f00, A0]
-    u = ricker.R(tt, L, *m_real)
+    m_real_d = dict(L=L0, t0=tc0, f0=fc0, A=A0)
+    u = ricker.R(tt, **m_real_d)
 
     # Make some noiiiise
     noise = 0.25*np.max(np.abs(u))*np.random.normal(loc=0, scale=1.0, size=N)
@@ -31,15 +32,16 @@ def create_OT_Waveform():
 
     # Synthetic/Source
     # Shift relative to the observed data
-    dt = 5.0
+    dt = 2.0
 
     ts = tt + dt
     tc1 = tc0 + dt
-    f01 = 1.0
+    fc1 = 1.0
     A1 = 3.0
+    L1 = 1.9
 
-    m_init = [tc1, f01, A1]
-    us = ricker.R(ts, L, *m_init)
+    m_init_d = dict(L=L1, t0=tc1, f0=fc1, A=A1)
+    us = ricker.R(ts, **m_init_d)
 
     # Create waveform object
     otw = Waveform(tt, ut, ts, us)
@@ -100,6 +102,59 @@ def optfunc_LSQ(x, data):
     return C, G
 
 
+def envelope(array):
+    """Is the absolute value of the analytic signal, which is the
+    signal + j*hilbert(signal). So
+    envelope = sqrt[signal^2 + hilbert(signal)^2]. """
+    return np.abs(analytic_signal(array))
+
+
+def hilbert(array):
+    """Scipy.signal.hilbert actually computes the analytical signal. The
+    imaginary parrt of which is the hilbert transform"""
+    return np.imag(analytic_signal)
+
+
+def optfunc_LSQ_ENV(x, data):
+    '''
+        Routine to act as an interface with scipy.minimize(). Actions
+            - takes model parameters and builds rickerwavelet (forward problem) plus derivatives
+            - outputs L2 norm and gradient
+    '''
+    # Get input data
+    [tt, ut, L0, L1, fc, tc, p, _] = data  # get data block
+    dt = np.diff(tt)[0]
+    # Create model vector
+    tc1, A1 = x
+    us = ricker.R(tt, L=L1, t0=tc1, f0=fc, A=A1)
+    dudm = ricker.dRdm(tt, L=L1, t0=tc1, f0=fc, A=A1)
+    dudm = (dudm[0], dudm[2])
+
+    # Compute envelopes
+    eus = envelope(us)
+    hus = hilbert(us)
+    hdudm = (hilbert(_dudm) for _dudm in dudm)
+
+    edudm = (
+        1/eus * (us * _dudm + hus * _hdudm)
+        for _dudm, _hdudm in zip(dudm, hdudm)
+    )
+
+    # Cost
+    Cwf = 0.5*np.sum((us-ut)**2*dt)
+    Cenv = 0.5 * np.sum((envelope(us)-envelope(ut))**2*dt)
+    C = Cwf + Cenv
+
+    # Gradient
+    Gwf = np.array([np.sum(dt*(us-ut) * _dudm) for _dudm in dudm])
+    Genv = np.array([
+        dt * np.sum((envelope(us)-envelope(ut)) * _edudm)
+        for _edudm in edudm])
+    G = Gwf + Genv
+
+    return C, G
+
+
 def optfunc_full(x, data):
     '''
         Routine to act as an interface with scipy.minimize(). Actions
@@ -143,6 +198,59 @@ def optfunc_LSQ_full(x, data):
     tc1, fc1, A1, L1 = x
     us = ricker.R(tt, L=L1, t0=tc1, f0=fc1, A=A1)
     dudm = ricker.dRdm(tt, L=L1, t0=tc1, f0=fc1, A=A1)
+
+    # Cost
+    C = 0.5*np.sum((us-ut)**2*dt)
+
+    # Gradient
+    G = np.array([np.sum(dt*(us-ut) * _dudm) for _dudm in dudm])
+
+    return C, G
+
+
+def optfunc_sambridge(x, data):
+    '''
+        Routine to act as an interface with scipy.minimize(). Actions
+            - takes model parameters and builds rickerwavelet (forward problem) plus derivatives
+            - takes rickerwavelet and builds fingerprint waveform object and OT object of 2D fingerprint density function.
+            - takes OT object and calculates Wasserstein misfits between time and amplitude marginals together with derivatives.
+            - Combines derivatives using the chain rule and returns average Wasserstein misfit for time-amplitude marginals plus derivatives.
+
+    '''
+    # Get input data
+    [tt, ut, L1, p, s, alpha, Nt, Nu, tangentnorm] = data  # get data block
+
+    # Create model vector
+    tc1, fc1, A1 = x
+    us = ricker.R(tt, L=L1, t0=tc1, f0=fc1, A=A1)
+    dudm = ricker.dRdm(tt, L=L1, t0=tc1, f0=fc1, A=A1)[0:3]
+
+    # Optimal Transport waveform comparison tool
+    otw = Waveform(tt, ut, tt, us, p=p, s=s, alpha=alpha,
+                   Nt=Nt, Nu=Nu, tangentnorm=tangentnorm)
+    otw.fingerprints()
+    otw.probabilities()
+    W = otw.wasser()
+    otw.dWduk()
+    deriv = otw.compute_dWdm(dudm)
+
+    return W[0], deriv
+
+
+def optfunc_LSQ_sambridge(x, data):
+    '''
+        Routine to act as an interface with scipy.minimize(). Actions
+            - takes model parameters and builds rickerwavelet (forward problem) plus derivatives
+            - outputs L2 norm and gradient
+    '''
+    # Get input data
+    [tt, ut, L1, _, _, _, _, _, _] = data  # get data block
+    dt = np.diff(tt)[0]
+
+    # Create model vector
+    tc1, fc1, A1 = x
+    us = ricker.R(tt, L=L1, t0=tc1, f0=fc1, A=A1)
+    dudm = ricker.dRdm(tt, L=L1, t0=tc1, f0=fc1, A=A1)[0:3]
 
     # Cost
     C = 0.5*np.sum((us-ut)**2*dt)

src/lwsspy/OT/ot.py
@@ -211,8 +211,8 @@ class Waveform:
             du = np.diff(self.unorm)
 
             # Compute ubar
-            ubart = 1/du * (self.ut - self.unorm[0] - self.unorm[1])
-            ubars = 1/du * (self.us - self.unorm[0] - self.unorm[1])
+            ubart = 1/du * (2*self.ut - self.unorm[0] - self.unorm[1])
+            ubars = 1/du * (2*self.us - self.unorm[0] - self.unorm[1])
 
             # Scale the waveforms
             self.utn = 0.5 + 1/np.pi * np.arctan(ubart)
@@ -354,13 +354,14 @@ class Waveform:
         self.dWudm = np.zeros(Nm)
 
         for i, dudmi in enumerate(dudm):
+
             self.dWudm[i] = np.sum(
-                self.dWuduk * dudmi[self.idk] +
-                self.dWudukp1 * dudmi[self.idk])
+                (self.dWuduk * dudmi[self.idk]) +
+                self.dWudukp1 * dudmi[self.idk+1])
 
             self.dWtdm[i] = np.sum(
                 self.dWtduk * dudmi[self.idk] +
-                self.dWtdukp1 * dudmi[self.idk])
+                self.dWtdukp1 * dudmi[self.idk+1])
 
         self.dWdm = self.alpha * self.dWtdm + (1-self.alpha) * self.dWudm
 

src/lwsspy/OT/plot.py
@@ -1,4 +1,6 @@
 import imp
+
+from matplotlib.cm import ScalarMappable
 from . import ot
 import matplotlib.pyplot as plt
 import numpy as np
@@ -6,14 +8,21 @@ from .. import plot as lplt
 
 
 def plot_OTWaveform(otw: ot.Waveform):
-    _, axes = plt.subplots(2, 2, figsize=(6, 6))
+    _, axes = plt.subplots(2, 2, figsize=(8, 5))
     plt.subplots_adjust(wspace=0.1, hspace=0.3)
+
+    extension = 0.05
+    utmin, utmax = np.min(otw.ut), np.max(otw.ut)
+    usmin, usmax = np.min(otw.us), np.max(otw.us)
+    umin, umax = np.minimum(utmin, usmin), np.maximum(utmax, usmax)
+    du = umax - umin
+    ulims = umin - 0.5 * extension * du, umax + 0.5 * extension * du
 
     # Plot observed
     axes[0, 0].plot([np.min(otw.tt), np.max(otw.tt)], [0, 0], 'k:', lw=0.75)
     axes[0, 0].plot(otw.tt, otw.ut)
     axes[0, 0].set_xlim(np.min(otw.tt), np.max(otw.tt))
-    axes[0, 0].set_ylim(otw.unorm[0], otw.unorm[1])
+    axes[0, 0].set_ylim(ulims)
     axes[0, 0].set_xlabel(f'$t$ [s]')
     axes[0, 0].set_ylabel(f'Amplitude $u$')
     axes[0, 0].set_title('Target: Observed')
@@ -22,7 +31,7 @@ def plot_OTWaveform(otw: ot.Waveform):
     axes[0, 1].plot([np.min(otw.ts), np.max(otw.ts)], [0, 0], 'k:', lw=0.75)
     axes[0, 1].plot(otw.ts, otw.us)
     axes[0, 1].set_xlim(np.min(otw.ts), np.max(otw.ts))
-    axes[0, 1].set_ylim(otw.unorm[0], otw.unorm[1])
+    axes[0, 1].set_ylim(ulims)
     axes[0, 1].set_xlabel(f'$t$ [s]')
     axes[0, 1].set_title('Source: Synthetic')
 
@@ -93,6 +102,52 @@ def plot_OTW_FP(otw: ot.Waveform):
                     colors='k', linewidths=0.25, levels=30)
 
 
+def plot_OTW_2D_PDF(otw: ot.Waveform):
+
+    if otw.fingerprint_computed is False:
+        otw.fingerprints()
+
+    # Create figure
+    fig, axes = plt.subplots(1, 2, figsize=(8, 4))
+    # subplots_adjust(wspace=0.4)
+
+    # #### BOTTOM ROW scaled and fingerprinted
+    # Observed
+    # axes[0].plot([np.min(tn0), np.max(tn0)], [baseline, baseline], 'k:', lw=0.75)
+    axes[0].plot(otw.ttn, otw.utn, 'k')
+    axes[0].set_xlim(np.min(otw.ttn), np.max(otw.ttn))
+    axes[0].set_title('Observed/Target')
+
+    # Predicted
+    # axes[1].plot([np.min(tn1), np.max(tn1)], [baseline, baseline], 'k:', lw=0.75)
+    axes[1].plot(otw.tsn, otw.usn, 'k')
+    axes[1].set_xlim(np.min(otw.tsn), np.max(otw.tsn))
+    axes[1].set_title('Synthetic/Source')
+
+    # Set limits
+    axes[0].set_ylim(0, 1)
+    axes[1].set_ylim(0, 1)
+
+    # Setting ticklabels
+    axes[1].tick_params(labelleft=False, labelright=True)
+    axes[0].tick_params(labelleft=False)
+    axes[1].tick_params(labelleft=False)
+
+    # Setting axes labels
+    axes[0].set_xlabel('Time $t\prime$ [s]')
+    axes[1].set_xlabel('Time $t\prime$ [s]')
+    axes[0].set_ylabel('Amplitude $u\prime$')
+
+    # Plot contours
+    pc = axes[0].pcolormesh(
+        otw.dtt, otw.dut, otw.ptn.T, cmap='rainbow', vmin=0, vmax=np.max(otw.ptn))
+    axes[1].pcolormesh(
+        otw.dts, otw.dus, otw.psn.T, cmap='rainbow', vmin=0, vmax=np.max(otw.ptn))
+    fig.colorbar(
+        ScalarMappable(norm=pc.norm, cmap=pc.cmap), ax=axes,
+        orientation='horizontal', fraction=0.05, shrink=0.6, aspect=40, pad=0.2)
+
+
 def plot_OTW_FP_MARG(otw: ot.Waveform):
 
     # Create figure
@@ -107,7 +162,7 @@ def plot_OTW_FP_MARG(otw: ot.Waveform):
 
     # Predicted
     # axes[1].plot([np.min(tn1), np.max(tn1)], [baseline, baseline], 'k:', lw=0.75)
-    axes[0, 1].plot(otw.ts, otw.us, 'k')
+    axes[0, 1].plot(otw.ts, otw.us, 'r')
     axes[0, 1].set_xlim(np.min(otw.ts), np.max(otw.ts))
     axes[0, 1].set_title('Synthetic/Source')
 
@@ -122,7 +177,7 @@ def plot_OTW_FP_MARG(otw: ot.Waveform):
 
     # Predicted
     # axes[1].plot([np.min(tn1), np.max(tn1)], [baseline, baseline], 'k:', lw=0.75)
-    axes[1, 1].plot(otw.tsn, otw.usn, 'k')
+    axes[1, 1].plot(otw.tsn, otw.usn, 'r')
     axes[1, 1].set_xlim(np.min(otw.tsn), np.max(otw.tsn))
 
     # Set limits
@@ -190,15 +245,15 @@ def plot_OTW_FP_MARG(otw: ot.Waveform):
     sourceyax.spines.top.set_visible(False)
 
     # Set labels
-    targetxax.set_xlabel("Time $t\prime$ [s]")
-    sourcexax.set_xlabel("Time $t\prime$ [s]")
+    targetxax.set_xlabel("Time $t\prime$")
+    sourcexax.set_xlabel("Time $t\prime$")
     targetyax.set_ylabel('Amplitude $u\prime$')
 
     # Plot marginal distributions
     targetxax.fill_between(otw.MargT.y, otw.MargT.gn, color=(0.8, 0.8, 0.8))
     targetyax.fill_betweenx(otw.MargU.y, otw.MargU.gn, color=(0.8, 0.8, 0.8))
-    sourcexax.fill_between(otw.MargT.x, otw.MargT.fn, color=(0.8, 0.8, 0.8))
-    sourceyax.fill_betweenx(otw.MargU.x, otw.MargU.fn, color=(0.8, 0.8, 0.8))
+    sourcexax.fill_between(otw.MargT.x, otw.MargT.fn, color=(0.9, 0.7, 0.7))
+    sourceyax.fill_betweenx(otw.MargU.x, otw.MargU.fn, color=(0.9, 0.7, 0.7))
 
     lplt.plot_label(axes[0, 0], 'a)', location=6, box=False)
     lplt.plot_label(axes[0, 1], 'b)', location=6, box=False)
@@ -215,7 +270,7 @@ def plot_OTW_FP_MARG_COMP(otw: ot.Waveform, noFP=True):
     # #### TOP ROW unscaled #####
     axes[0].plot(otw.tt, otw.ut, 'k', label='Observed')
     axes[0].set_xlim(np.min(otw.tt), np.max(otw.tt))
-    axes[0].set_title('Source(red) and Target (black) after inversion')
+    # axes[0].set_title('Target (black) and Source(red) ')
 
     # Predicted
     # axes[1].plot([np.min(tn1), np.max(tn1)], [baseline, baseline], 'k:', lw=0.75)
@@ -231,14 +286,14 @@ def plot_OTW_FP_MARG_COMP(otw: ot.Waveform, noFP=True):
 
     # #### BOTTOM ROW scaled and fingerprinted
     # Observed
-    axes[1].plot(otw.ttn, otw.utn, 'k')
+    axes[1].plot(otw.ttn, otw.utn, 'k', label='Target/Obs')
     axes[1].set_xlim(np.min(otw.ttn), np.max(otw.ttn))
 
     # Predicted
     # axes[1].plot([np.min(tn1), np.max(tn1)], [baseline, baseline], 'k:', lw=0.75)
-    axes[1].plot(otw.tsn, otw.usn, 'r')
+    axes[1].plot(otw.tsn, otw.usn, 'r', label='Source/Syn')
     axes[1].set_xlim(np.min(otw.tsn), np.max(otw.tsn))
-
+    axes[1].legend(loc='upper left', frameon=False)
     # Set limits
     axes[1].set_ylim(0, 1)
 
@@ -281,7 +336,7 @@ def plot_OTW_FP_MARG_COMP(otw: ot.Waveform, noFP=True):
     pyax.spines.top.set_visible(False)
 
     # Set labels
-    pxax.set_xlabel("Time $t\prime$ [s]")
+    pxax.set_xlabel("Time $t\prime$")
     pyax.set_ylabel('Amplitude $u\prime$')
 
     # Plot marginal distributions
@@ -316,7 +371,7 @@ def plot_OTW_FP_D(otw: ot.Waveform):
     # axes[1].tick_params(labelleft=False)
 
     # Setting axes labels
-    axes[3].set_xlabel('Time $t\prime$ [s]')
+    axes[3].set_xlabel('Time $t\prime$')
 
     # Setting titles
     axes[0].set_title('$d_{ij}$')
@@ -351,9 +406,10 @@ def plot_rays(otw: ot.Waveform, npts: int = 250):
 
     lw = 1.0
     ls = '-'
-    ax.plot(otw.tsn, otw.usn, 'k', lw=lw, ls=ls)
+    x, = ax.plot(otw.tsn, otw.usn, 'k', lw=lw, ls=ls,
+                 label=r'$\mathbf{x}(\lambda, \mathbf{x}_k, \mathbf{x}_{k+1})$')
     ax.set_ylim(0, 1)
-    ax.set_xlabel('Time $t\prime$ [s]')
+    ax.set_xlabel('Time $t\prime$')
     ax.set_ylabel('Amplitude $u\prime$')
 
     # Get random rays
@@ -375,11 +431,12 @@ def plot_rays(otw: ot.Waveform, npts: int = 250):
     pusk = otw.dus[jds].flatten()
 
     #
-    ax.plot(np.vstack((ptsk, tlam)), np.vstack(
-        (pusk, ulam)), c=(0.8, 0.2, 0.2), lw=0.5)
-    ax.scatter(ptsk, pusk, c='k', s=.5, zorder=10)
+    pl = ax.plot(np.vstack((ptsk, tlam)), np.vstack(
+        (pusk, ulam)), c=(0.8, 0.2, 0.2), lw=0.5, label='Distances')
+    ps = ax.scatter(ptsk, pusk, c='k', s=.5, zorder=10, label=r'$\mathbf{p}$')
     ax.scatter(tlam, ulam, c='k', s=.5, zorder=10)
     ax.set_xlim(np.min(otw.tsn), np.max(otw.tsn))
+    plt.legend(handles=(pl[0], ps, x), loc='lower left')
 
 
 def plot_lambdas(otw: ot.Waveform):
@@ -395,7 +452,7 @@ def plot_lambdas(otw: ot.Waveform):
     ax.plot(otw.tsn, otw.usn, 'k', lw=lw, ls=ls)
     ax.set_xlim(np.min(otw.tsn), np.max(otw.tsn))
     ax.set_ylim(0, 1)
-    ax.set_xlabel('Time $t\prime$ [s]')
+    ax.set_xlabel('Time $t\prime$')
     ax.set_ylabel('Amplitude $u\prime$')
 
     p = ax.pcolormesh(